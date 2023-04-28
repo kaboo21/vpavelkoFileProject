2c9a6e18242ae28e5a77553f9c6425f32afb9f29
vpavelko <vpavelko@softjourn.com>
2023-04-28 15:31:56 +0300

    File password validation

--- vpavelkoFileProject/Pages/Index.cshtml.cs
+++ vpavelkoFileProject/Pages/Index.cshtml.cs
@@ -1,20 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using vpavelkoFileProject.models;
 
 namespace vpavelkoFileProject.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public List<PasswordDetails> passwordDetailsList { get; set; }
+        public int ValidPasswordNumber { get; set; }
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
+            passwordDetailsList = new List<PasswordDetails>();
             _logger = logger;
         }
 
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPostAsync(IFormFile file)
+        {
+            if(file is null)
+                return Page();
+            List<string> lines = new List<string>();
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                while (reader.Peek() >=0)
+                {
+                    lines.Add(reader.ReadLine());
+                }
+            }
+
+            lines.ForEach(line =>
+            {
+                var details = new PasswordDetails(line);
+                passwordDetailsList.Add(details);
+            });
+
+            ValidPasswordNumber = passwordDetailsList.Where(p => p.IsValidPassword).Count();
+
+            return Page();
         }
     }
 }
---
+++ vpavelkoFileProject/models/PasswordDetails.cs
@@ -0,0 +1,81 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace vpavelkoFileProject.models
+{
+    public class PasswordDetails
+    {
+        const int _validNumberOfWhiteSpace = 2;
+        private Regex _regex = new Regex(@"^.{1}\s\d*d?-\d*d?: .+");
+        private int _repeats;
+        public string Password { get; set; }
+        public int MinRepeats { get; set; }
+        public int MaxRepeats { get; set; }
+        public char Char { get; set; }
+        public bool IsValidImputLine { get; set; }
+        public bool IsValidPassword { get; set; }
+
+        public PasswordDetails(string passwordLine)
+        {
+            ParseStringLine(passwordLine);
+            PasswordValidation();
+        }
+
+        private void ParseStringLine(string passwordLine)
+        {
+            if (!_regex.IsMatch(passwordLine)) 
+                return;
+
+            IsValidImputLine = true;
+            
+            var passTrimed = passwordLine.Trim();
+            if(passTrimed.Count(Char.IsWhiteSpace) > _validNumberOfWhiteSpace)
+            {
+                IsValidImputLine = false;
+                return;
+            }
+
+            var separetedStrArr = passwordLine.Split();
+
+            if (separetedStrArr[0].Length > 1)
+            {
+                IsValidImputLine = false;
+                return;
+            }
+
+            //take char for validation
+            Char = separetedStrArr[0][0];
+
+            //take Min and Max number of repeats
+            var repeatsValuesStrArr = separetedStrArr[1].Split('-');
+
+            var strNum1 = repeatsValuesStrArr[0];
+            var strNum2 = repeatsValuesStrArr[1].TrimEnd(':');
+
+            if (int.TryParse(strNum1, out var num1) 
+                && int.TryParse(strNum2, out var num2))
+            {
+                if (num1 >= num2)
+                {
+                    IsValidImputLine = false;
+                    return;
+                }
+
+                MinRepeats = num1;
+                MaxRepeats = num2;
+            }
+
+            //take password
+            Password = separetedStrArr[2];
+        }
+
+        private void PasswordValidation()
+        {
+            if (!IsValidImputLine)
+                return;
+            _repeats = Password.Where(c => c == Char).Count();
+            if (_repeats >= MinRepeats && _repeats <= MaxRepeats) 
+                IsValidPassword = true;
+        }
+    }
+}
